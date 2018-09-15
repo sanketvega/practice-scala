4b2cb359b72db8d26274cdad52fbe0750dca62d3
seulchan <seulchan07@gmail.com>
2018-09-15 22:35:34 +0200

    Add Chart and Media codes

---
+++ codes/scalaFX/Graphics/Media.scala
@@ -0,0 +1,44 @@
+/*
+ This code is from a book 
+ <Introduction to Programming and Problem-solving Using Scala>
+ written by Mark C. Lewis
+*/
+import scalafx.Includes._
+import scalafx.application.JFXApp
+import scalafx.scene.Scene
+import scalafx.scene.media._
+import scalafx.scene.layout._
+import scalafx.scene.control.Button
+import scalafx.event.ActionEvent
+
+val app = new JFXApp {
+    stage = new JFXApp.PrimaryStage {
+      title = "Media"
+      scene = new Scene(600, 350) {
+        val audioClip = new AudioClip("http://www.largesound.com/ashborytour/sound/brobob.mp3")
+        val fireButton = new Button
+        fireButton.onAction = (ae:ActionEvent) => audioClip.play
+        fireButton.prefWidth = Int.MaxValue
+
+        val media = new Media("http://techslides.com/demos/sample-videos/small.mp4")
+        val mediaPlayer = new MediaPlayer(media)
+        val mediaView = new MediaView(mediaPlayer)
+
+        val playButton = new Button("Play")
+        playButton.onAction = (ae:ActionEvent) => mediaPlayer.play
+        val pauseButton = new Button("Pause")
+        pauseButton.onAction = (ae:ActionEvent) => mediaPlayer.pause
+        val stopButton = new Button("Stop")
+        stopButton.onAction = (ae:ActionEvent) => mediaPlayer.stop
+
+        val borderPane = new BorderPane
+        borderPane.top = fireButton
+        borderPane.left = new VBox(playButton, pauseButton, stopButton)
+        borderPane.center = mediaView
+
+        root = borderPane
+
+      }
+    }
+  }
+  app.main(args)
